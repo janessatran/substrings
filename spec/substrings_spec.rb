08f4378f7effe71990d2e87d1c2ef0492e4d99ac
Janessa Tran <tranjanessa@gmail.com>
2019-02-25 21:44:27 -0600

    add changes to dict spacing and code block spacing

--- spec/substrings_spec.rb
+++ spec/substrings_spec.rb
@@ -3,7 +3,20 @@ require './substrings'
 describe '#substrings' do
 
   it 'takes a string and finds all the words + its substrings and returns how many times it appears' do
-    dict = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+    dict = ["below",
+            "down",
+            "go",
+            "going",
+            "horn",
+            "how",
+            "howdy",
+            "it",
+            "i",
+            "low",
+            "own",
+            "part",
+            "partner",
+            "sit"]
     expect((substrings("below", dict))).to eq({ "below" => 1, "low" => 1 })
   end
 
